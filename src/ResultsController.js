18a2af32154b0ae1fa6cb303086f5573ae22fea4
Will Abson <will.abson@gmail.com>
2015-12-30 23:46:07 +0000

    Check finish times function

--- src/ResultsController.js
+++ src/ResultsController.js
@@ -140,6 +140,56 @@ function saveEntriesHTMLForSpreadsheet(ssKey) {
   }, SpreadsheetApp.openById(ssKey));
 }
 
+function checkFinishDuplicates_(ss) {
+  var sheet = ss.getSheetByName('Finishes');
+  if (sheet === null) {
+    throw 'Finishes sheet not found';
+  }
+  var data = getTableRows(sheet, true), timesByBoatNum = {}, strangeTimes = [], times, number, bn, time;
+  for (var i=0; i<data.length; i++) {
+    number = data[i]['number'];
+    bn = '' + data[i]['boat num'];
+    time = formatTime(data[i]['time']);
+    if (bn !== '') {
+      times = timesByBoatNum[bn] || [];
+      times.push({
+        number: number,
+        time: time
+      });
+      timesByBoatNum[bn] = times;
+      if (!/\d{3}/.test(bn) || !/^\d{1,2}:\d{2}:\d{2}|rtd|dns|dsq$/.test(time)) {
+        strangeTimes.push({
+          number: number,
+          boatNumber: bn,
+          time: time
+        });
+      }
+    }
+  }
+  var duplicateBns = [];
+  for (var num in timesByBoatNum) {
+    if (timesByBoatNum.hasOwnProperty(num) && timesByBoatNum[num].length > 1) {
+      duplicateBns.push({
+        boatNumber: num,
+        times: timesByBoatNum[num]
+      });
+    }
+  }
+  return {
+    duplicates: duplicateBns,
+    strangeTimes: strangeTimes
+  };
+}
+
+function checkFinishDuplicatesForSpreadsheet(ssKey) {
+  var ss = SpreadsheetApp.openById(ssKey);
+  if (ss !== null) {
+    return checkFinishDuplicates_(ss);
+  } else {
+    throw 'Could not find spreadsheet with key ' + ssKey;
+  }
+}
+
 /**
  * List HRM files
  *
