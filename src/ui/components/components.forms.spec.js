6425a9002fef7fd7aee27a354ab8f2765eca7b57
Will Abson <will.abson@gmail.com>
2016-06-29 21:50:22 +0100

    Fix missing semicolon at end of line

--- src/ui/components/components.forms.spec.js
+++ src/ui/components/components.forms.spec.js
@@ -15,7 +15,7 @@ describe('Forms components', function() {
                 blocks: [new MyBlock({id: 'a'}), new MyBlock({id: 'b'}), new MyBlock({id: 'c'})]
             });
             this.view.render();
-            expect(this.view.el).toContainHtml('<div id="a"></div><div id="b"></div><div id="c"></div>')
+            expect(this.view.el).toContainHtml('<div id="a"></div><div id="b"></div><div id="c"></div>');
         });
     });
 
