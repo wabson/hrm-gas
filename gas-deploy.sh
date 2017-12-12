ad1166be74e0b1a06490f1885780f21590510eec
Will Abson <will.abson@gmail.com>
2017-12-12 11:24:12 +0000

    Push GAS code from Travis

---
+++ gas-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d
+        npm run upload-test
+    fi
+fi
