3ee0b9384a39960de5871f1fbf67706f2556d745
Will Abson <will.abson@gmail.com>
2017-12-12 11:34:07 +0000

    Create directory build as well as build/test

--- gas-deploy.sh
+++ gas-deploy.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
-        mkdir build/test && \
+        mkdir -p build/test && \
             openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d && \
             npm run upload-test
     fi
