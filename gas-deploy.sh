1aec2a7805b741b71b2d8c6b09641758baf65aa9
Will Abson <will.abson@gmail.com>
2017-12-12 15:27:41 +0000

    Add .gapps file required for publishing to Drive

--- gas-deploy.sh
+++ gas-deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+deploy_to_project () {
+    openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in .gapps-travis.enc -out $HOME/.gapps -d && \
+        mkdir -p build/${1} && \
+        printf "{\n  \"path\": \"includes\",\n  \"fileId\": \"${2}\"\n}" > build/${1}/gapps.config.json && \
+        npm run upload-${1}
+}
+
 if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
-        mkdir -p build/test && \
-            openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d && ls -l build/test && \
-            npm run upload-test
+        deploy_to_project test 1q0YEwJXEZTbKT1Bnros3NWj9PZxjT8wB6Xqg1mNb4BSDa685a9oQdrFV
     fi
 fi
