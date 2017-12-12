4269ee243e146bb2ea4cc31c4a2021db6818cdb5
Will Abson <will.abson@gmail.com>
2017-12-12 12:53:31 +0000

    List directory contents to aid with debugging

--- gas-deploy.sh
+++ gas-deploy.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         mkdir -p build/test && \
-            openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d && \
+            openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d && ls -l build/test && \
             npm run upload-test
     fi
 fi
