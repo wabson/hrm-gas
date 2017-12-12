5b366d822f344d1e0ef928eb56a498dc1f4840fb
Will Abson <will.abson@gmail.com>
2017-12-12 11:30:04 +0000

    Ensure config dir exists before uploading GAS code

--- gas-deploy.sh
+++ gas-deploy.sh
@@ -2,7 +2,8 @@
 
 if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
-        openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d
-        npm run upload-test
+        mkdir build/test && \
+            openssl aes-256-cbc -K $encrypted_238fd4bcbc3a_key -iv $encrypted_238fd4bcbc3a_iv -in gapps.config.json.enc -out build/test/gapps.config.json -d && \
+            npm run upload-test
     fi
 fi
